04e421cbbfa4967a9c06f5e513548203bf48b594
장근영 <jky00914@naver.com>
2024-05-01 17:28:47 +0900

    formLogin

---
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -0,0 +1,23 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/loginPage")
+    public String login() {
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
