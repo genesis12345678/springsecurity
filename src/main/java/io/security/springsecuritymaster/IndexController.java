ce7faefe84d07603182e89c4d84a4df0fe141783
장근영 <jky00914@naver.com>
2024-05-08 20:10:43 +0900

    Authorization

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,14 +1,6 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,83 +24,5 @@ public class IndexController {
     @GetMapping("/admin")
     public String admin() {
         return "admin";
-    }
-
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/denied")
-    public String denied() {
-        return "denied";
-    }
-
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
-
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
-    }
-
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "not anonymous";
-        }
-    }
-
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
-
-        return context.getAuthentication().getName();
-    }
-
-    @GetMapping("/logoutSuccess")
-    public String logoutSuccess() {
-        return "logoutSuccess";
-    }
-
-    @GetMapping("/invalidSession")
-    public String invalidSession() {
-        return "invalidSession";
-    }
-
-    @GetMapping("/expiredSession")
-    public String expiredSession() {
-        return "expiredSession";
-    }
-
-    @PostMapping("/csrf")
-    public String csrf() {
-        return "csrf 적용됨";
-    }
-
-    @PostMapping("/formCsrf")
-    public CsrfToken formCsrf(CsrfToken csrfToken) {
-        return csrfToken;
-    }
-
-    @PostMapping("/cookieCsrf")
-    public CsrfToken cookieCsrf(CsrfToken csrfToken) {
-        return csrfToken;
-    }
-
-    @GetMapping("/csrfToken")
-    public String csrfToken(HttpServletRequest request) {
-        CsrfToken csrfToken1 = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        CsrfToken csrfToken2 = (CsrfToken) request.getAttribute("_csrf");
-
-        return csrfToken1.getToken();
     }
 }
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,51 +1,23 @@
 package io.security.springsecuritymaster;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.util.Introspection;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.beans.Introspector;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
-@Slf4j
+@EnableMethodSecurity
 public class SecurityConfig {
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> {
-            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-        };
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -63,31 +35,25 @@ public class SecurityConfig {
     }
 
     @Bean
-    public RoleHierarchy roleHierarchy() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_DB\n" +
-                                   "ROLE_DB > ROLE_USER\n" +
-                                   "ROLE_USER > ROLE_ANONYMOUS");
-
-        return roleHierarchy;
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("MYPREFIX_");
     }
-
 
    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user")
                 .password("{noop}1111")
-                .roles("USER")
+                .authorities("MYPREFIX_USER")
                 .build();
 
         UserDetails manager = User.withUsername("db")
                 .password("{noop}1111")
-                .roles("DB")
+                .authorities("MYPREFIX_DB")
                 .build();
 
        UserDetails admin = User.withUsername("admin")
                .password("{noop}1111")
-               .roles("ADMIN", "SECURE")
+               .authorities("MYPREFIX_ADMIN", "MYPREFIX_SECURE")
                .build();
 
         return new InMemoryUserDetailsManager(user, manager, admin);
