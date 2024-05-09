a2eef81792986f6a199d9ce339c8a26f6d369e90
장근영 <jky00914@naver.com>
2024-05-09 11:31:07 +0900

    CustomRequestMatcherDelegatingAuthorizationManager

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,6 +2,9 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +17,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,15 +36,38 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
-                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
-                        .anyRequest().authenticated())
+                        .anyRequest().access(authorizationManager(null)))
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
+    }
+
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(HandlerMappingIntrospector introspector) {
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/user"),
+                                                    AuthorityAuthorizationManager.hasAuthority("ROLE_USER"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/db"),
+                        AuthorityAuthorizationManager.hasAuthority("ROLE_DB"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/admin"),
+                        AuthorityAuthorizationManager.hasAuthority("ROLE_ADMIN"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4 =
+                new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
+
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+        mappings.add(requestMatcherEntry4);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
     }
 
    @Bean
